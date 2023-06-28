ef80fab10c6b44c612d355d20f057bc319aaa454
Iago Fernandes <iago.avila@ufvjm.edu.br>
2023-06-28 15:03:29 -0300

    Alteração no cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
 
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
@@ -13,6 +15,9 @@ app.use((req, res, next) => {
      "Access-Control-Allow-Headers",
      "Origin, X-Requested-With, Content-Type, Accept"
    );
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+   app.use(cors())
    next();
  })
 
